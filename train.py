bdef678182261dc1107ba0d8a0ebd4cbe45b7310
Taloce117 <azertom3186@gmail.com>
2024-05-08 15:05:39 +0100

    Copy du cotenue du prof

--- train.py
+++ train.py
@@ -4,28 +4,40 @@ from sklearn.base import ClassifierMixin
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+
 def ingest_data(file_path: str) -> pd.DataFrame:
     return pd.read_excel(file_path)
 
+
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    # suppresion des lignes avec des valeurs manquantes
-    df = df.dropna(inplace=True)
-    print(df)
+    # suppression des lignes avec des valeurs manquantes
+    df = df[['survived', 'pclass', 'sex', 'age']]
+    df.dropna(axis=0, inplace=True)
+
     # remplacer les valeurs non numériques par des valeurs numériques
-    df = df.replace('female', 0).replace('male', 1)
+    df['sex'].replace(to_replace=['male', 'female'], value=[0, 1], inplace=True)
+
     return df
 
+
 def train_model(df: pd.DataFrame) -> ClassifierMixin:
+    # instantiate model
     model = KNeighborsClassifier(4)
+
+    # split data
     y = df['survived']
-    x = df.drop['survived']
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    X = df.drop('survived', axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
     # train model
-    model.fit(x,y)
-    # evaluate model
+    model.fit(X_train, y_train)
+
+    # Evaluate model
     score = model.score(X_test, y_test)
-    print(f"score: {score}")
+    print(f"Model score: {score}")
+
     return model
+
 
 if __name__ == "__main__":
     df = ingest_data("titanic.xls")
